09f71d8f6d4ad8146b077bb86a780397de0c4591
ShiinaOrez <shiina_orez@qq.com>
2019-03-02 00:37:33 +0800

    [init]first exsercise

---
+++ exercise1/public/js/dispatch.js
@@ -0,0 +1,15 @@
+function baseInfo() {
+    window.location.href = "/public/html/baseInfo.html"
+}
+
+function familyInfo() {
+    window.location.href = "/public/html/familyInfo.html"
+}
+
+function eduHistory() {
+    window.location.href = "/public/eduHistory.html"
+}
+
+function hobby() {
+    window.location.href = "/public/hobby.html"
+}
