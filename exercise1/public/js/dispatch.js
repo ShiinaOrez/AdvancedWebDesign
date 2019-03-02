6fb133a4c08d479fe97f5d54ec4fed982ad0618b
ShiinaOrez <shiina_orez@qq.com>
2019-03-02 19:31:36 +0800

    [feat]

--- exercise1/public/js/dispatch.js
+++ exercise1/public/js/dispatch.js
@@ -1,15 +1,15 @@
 function baseInfo() {
-    window.location.href = "/public/html/baseInfo.html"
+    window.location.href = "./public/html/baseInfo.html"
 }
 
 function familyInfo() {
-    window.location.href = "/public/html/familyInfo.html"
+    window.location.href = "./public/html/familyInfo.html"
 }
 
 function eduHistory() {
-    window.location.href = "/public/eduHistory.html"
+    window.location.href = "./public/html/eduHistory.html"
 }
 
 function hobby() {
-    window.location.href = "/public/hobby.html"
+    window.location.href = "./public/html/hobby.html"
 }
